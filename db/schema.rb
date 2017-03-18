c60372f088d69e3e079cd861f659a570247076fa
JWade34 <justinwmckelvey@gmail.com>
2017-03-18 13:55:25 -0500

    postgres junk

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170318165328) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "counters", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
